86eeb6bbb8939f67f8e6e35f5e6e828b6e4211e2
Doublc-Qluv <963880465@qq.com>
2020-01-16 20:06:34 +0800

    Score

--- playML/kNN.py
+++ playML/kNN.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import sqrt
 from collections import Counter
+from playML.metrics import accuracy_score
 
 class kNNClassifier:
     def __init__(self,k):
@@ -43,5 +44,11 @@ class kNNClassifier:
         
         return votes.most_common(1)[0][0]
     
+    def score(self, X_test, y_test):
+        """根据测试数据集 X_test 和 y_test 确定当前模型的准确度"""
+
+        y_predict = self.predict(X_test)
+        return accuracy_score(y_test, y_predict)
+
     def __repr__(self):
         return "kNN(k=%d)" % self.k
