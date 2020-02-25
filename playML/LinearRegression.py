d92cbcf9b4994e7a7a5cc64c16688d5ac783b1eb
Doublc-Qluv <963880465@qq.com>
2020-02-25 14:09:16 +0800

    6-GradientDescent

--- playML/LinearRegression.py
+++ playML/LinearRegression.py
@@ -18,6 +18,82 @@ class LinearRegression:
         self.coef_ = self._theta[1:]
         return self
 
+    def fit_gd(self, X_train, y_train, eta=0.01, n_iters=1e4, epsilon=1e-8):
+        """使用梯度下降法训练线性回归模型"""
+        assert X_train.shape[0] == y_train.shape[0], \
+            "the size of X_train must be equal to the size of y_train"
+
+        def J(theta, X_b, y):
+            try:
+                return np.sum((y - X_b.dot(theta))**2) / len(y)
+            except:
+                return float('inf')
+
+        def dJ(theta, X_b, y):
+            # res = np.empty(len(theta))
+            # res[0] = np.sum(X_b.dot(theta) - y)
+            # for i in range(1, len(theta)):
+            #     res[i] = (X_b.dot(theta) - y).dot(X_b[:, i])  # sum?
+            # return res * 2 / len(X_b)
+            return X_b.T.dot(X_b.dot(theta) - y) * 2./len(y)
+
+        def gradient_descent(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
+            theta = initial_theta
+            # theta_history.append(initial_theta)
+            i_iter = 0
+
+            while i_iter < n_iters:
+                gradient = dJ(theta, X_b, y)
+                last_theta = theta
+                theta = theta - eta * gradient
+                # theta_history.append(theta)
+
+                if (abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon):
+                    break
+                i_iter += 1
+            return theta
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+        self._theta = gradient_descent(\
+            X_b, y_train, initial_theta, eta, n_iters)
+        self.interception_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+
+        return self
+
+    def fit_sgd(self, X_train, y_train, n_iters=5, t0=5, t1=50):
+
+        assert X_train.shape[0] == y_train.shape[0],\
+            "size of X_train must be equal to size of y_train"
+        assert n_iters >= 1
+
+        def dJ_sgd(theta, X_b_i, y_i):
+            return X_b_i * (X_b_i.dot(theta)-y_i)*2
+
+        def sgd(X_b, y, initial_theta, n_iters, t0=5, t1=50):
+            def learning_rate(t):
+                return t0/(t+t1)
+            theta = initial_theta
+            m = len(X_b)
+            for cur_iter in range(n_iters):
+                indexes = np.random.permutation(m)
+                X_b_new = X_b[indexes]
+                y_new = y[indexes]
+                for i in range(m):
+                    #rand_i = np.random.randint(m)
+                    gradient = dJ_sgd(theta, X_b_new[i], y_new[i])
+                    theta = theta-learning_rate(cur_iter*m+i)*gradient
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.random.randn(X_b.shape[1])
+        self._theta = sgd(X_b, y_train, initial_theta, n_iters, t0, t1)
+
+        self.interception_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+
+        return self
+
     def predict(self, X_predict):
         """预测"""
         assert self.interception_ is not None and self.coef_ is not None, \
